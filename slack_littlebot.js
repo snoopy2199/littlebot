492cdb5828d8337ba8b87192a8b089f6ac119f00
snoopy2199 <snoopy2199@gmail.com>
2016-10-20 10:34:57 +0800

    update function

--- slack_littlebot.js
+++ slack_littlebot.js
@@ -316,7 +316,7 @@ controller.hears([':heclap:'], 'direct_message,direct_mention,mention,ambient', 
     bot.reply(message, ":heclap:");
 });
 
-controller.hears(['好棒', '太棒', '超棒'], 'direct_message,direct_mention,mention', function(bot, message) {
+controller.hears(['好棒', '太棒', '超棒', '真棒', '棒棒'], 'direct_message,direct_mention,mention', function(bot, message) {
     bot.reply(message, "因為你的一句讚美讓littlebot變得更好 :heshy2:");
 });
 
