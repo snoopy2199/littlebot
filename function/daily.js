b3ff455cd7be121a57b0721a625a4332ea2a1576
snoopy2199 <snoopy2199@gmail.com>
2017-08-17 18:23:55 +0800

    Fix daily function with cheerio

--- function/daily.js
+++ function/daily.js
@@ -1,27 +1,24 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var cheerio = require("cheerio");
 
-module.exports = function(controller){
+module.exports = function(controller, bot){
     controller.hears(['Daily'], 'direct_message,direct_mention,mention', function(bot, message) {
-        getDaily(message);
+        getDaily(message, bot);
     });
 };
 
-function getDaily(message) {
-    var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D'http%3A%2F%2Fwww.dailyenglishquote.com%2F'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+function getDaily(message, bot) {
+    var url = "http://www.dailyenglishquote.com/";
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            var content = JSON.parse(this.responseText).query.results.body.div[3].div[1].div[0].div[2];
+            var $ = cheerio.load(this.responseText, {decodeEntities: false});
 
             var daily = {};
-            daily.quote = content.p[0].strong.content;
-            if (typeof(daily.quote) === "undefined") {
-                daily.quote = content.p[0].strong.span.content;
-            }
-
-            daily.from = content.div[2].content;
-            daily.chinese = content.p[1];
+            daily.quote = $(".entry.cf").eq(0).find("strong").text();
+            daily.from = $(".entry.cf").eq(0).find("div").eq(2).text();
+            daily.chinese = $(".entry.cf").eq(0).find("p").eq(1).text();
 
             bot.reply(message, ">>>" + daily.quote + "\n" + daily.from);
             bot.reply(message, daily.chinese);
